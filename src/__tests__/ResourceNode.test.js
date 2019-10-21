68219b4cf6d022e1228dd440e63a637cfb95a5af
GriffinJ <jrgriffiniii@gmail.com>
2019-10-21 16:07:40 -0500

    Gets the test passing

--- src/__tests__/ResourceNode.test.js
+++ src/__tests__/ResourceNode.test.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, {shallow} from 'enzyme';
+import Enzyme, {shallow, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({adapter: new Adapter()});
 import ResourceNode from '../containers/ResourceNode';
 
 it('renders the component on the DOM', () => {
-  const resource = shallow(<ResourceNode />);
-  expect(resource).toBeTruthy;
+  const resourceNode = mount(<ResourceNode classes={{root: 'bar'}} label="Foo" selected={false} />);
+  expect(resourceNode).toBeTruthy;
+
+  const container = resourceNode.find('.ResourceNode-root-1');
+  expect(container).toHaveClassName('.ResourceNode-root-1');
 });
